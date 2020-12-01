03dca0ca64877b933553b670a8f6be9b193a24db
Uche <48472791+ucheesther@users.noreply.github.com>
2020-12-01 01:10:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+  // fetch('https://my-json-server.typicode.com/ucheesther/fakeserver/db')
+  // .then(response => response.json())
+  // .then(json => console.log(json))
+
+//   var template = "";
+// fetch('https://my-json-server.typicode.com/ucheesther/fakeserver/db').then(response=> response.json()).then(data=>{
+// data.persons.forEach(person => {
+//   template+=`<div class="card">
+//             <div class="card-image waves-effect waves-block waves-light">
+//               <img class="activator" src="${person.imgUrl}">
+//           </div>
+//               <div class="card-content">
+//                 <span class="card-title activator grey-text text-darken-4">Edike Uche<i
+//                   class="material-icons right">more_vert</i></span>
+//                 <p>${person.email}</p>
+//               </div>
+//               <div class="card-reveal">
+//                 <span class="card-title grey-text text-darken-4">${person.name}<i class="material-icons right">close</i></span>
+//                 <p>Fullstack Developer</p>
+//               </div>
+//             </div>`
+            
+
+            
+  
+// });
+//   document.getElementById('maincontent').innerHTML = eval('`' + template + '`');
+
+// });
